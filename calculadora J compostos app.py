b53751adbc31cf6ddbc240035737d63b1f4b1f36
lasuli-prog <admpaularanielly@gmail.com>
2026-03-12 20:01:57 -0300

    Create calculadora J compostos app.py

---
+++ calculadora J compostos app.py
@@ -0,0 +1,10 @@
+st.header("Calculadora de Juros Compostos")
+
+capital = st.number_input("Capital")
+taxa = st.number_input("Taxa (%)")
+tempo = st.number_input("Tempo")
+
+montante = capital * (1 + taxa/100) ** tempo
+
+if st.button("Calcular montante"):
+  st.write("Montante:", montante) 
